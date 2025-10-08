abe62fa6d5051b8fc28923c9b6daf8b819f30541
Dmitry Maksimovich <1dimakss@gmail.com>
2025-10-08 20:48:40 +0500

    Add more careers

--- Hate.lua
+++ Hate.lua
@@ -1,5 +1,19 @@
 Hate = {
-  CareerAllowed = false
+  CareerAllowed = false,
+  ActiveUpdater = nil,
+}
+
+Hate.AllowedCareers = {
+  [GameData.CareerLine.BLACKGUARD] = true,
+  [GameData.CareerLine.IRON_BREAKER] = true,
+  [GameData.CareerLine.BRIGHT_WIZARD] = true,
+  [GameData.CareerLine.SORCERER] = true,
+  [GameData.CareerLine.WITCH_HUNTER] = true,
+  [GameData.CareerLine.WITCH_ELF] = true,
+  [GameData.CareerLine.CHOPPA] = true,
+  [GameData.CareerLine.SLAYER] = true,
+  [GameData.CareerLine.DISCIPLE] = true,
+  [GameData.CareerLine.WARRIOR_PRIEST] = true,
 }
 
 function Hate.Init()
@@ -11,8 +25,28 @@ function Hate.Enable()
   Hate.CareerAllowed = Hate.isCareerAllowed()
   if not Hate.CareerAllowed then return end
   Hate.ShowHateWindow()
-  RegisterEventHandler(SystemData.Events.PLAYER_CAREER_RESOURCE_UPDATED, "Hate.Update")
-  Hate.Update()
+  
+  if GameData.Player.career.line == GameData.CareerLine.DISCIPLE or GameData.Player.career.line == GameData.CareerLine.WARRIOR_PRIEST then
+    RegisterEventHandler(SystemData.Events.PLAYER_CAREER_RESOURCE_UPDATED, "Hate.UpdateDoC")
+    Hate.UpdateDoC()
+    Hate.ActiveUpdater = "DoC"
+  elseif GameData.Player.career.line == GameData.CareerLine.WITCH_HUNTER or GameData.Player.career.line == GameData.CareerLine.WITCH_ELF then
+    RegisterEventHandler(SystemData.Events.PLAYER_CAREER_RESOURCE_UPDATED, "Hate.UpdateWE")
+    Hate.UpdateWE()
+    Hate.ActiveUpdater = "WE"
+  elseif GameData.Player.career.line == GameData.CareerLine.BLACKGUARD or GameData.Player.career.line == GameData.CareerLine.IRON_BREAKER then
+    RegisterEventHandler(SystemData.Events.PLAYER_CAREER_RESOURCE_UPDATED, "Hate.UpdateBG")
+    Hate.UpdateBG()
+    Hate.ActiveUpdater = "BG"
+  elseif  GameData.Player.career.line == GameData.CareerLine.CHOPPA or GameData.Player.career.line == GameData.CareerLine.SLAYER then
+    RegisterEventHandler(SystemData.Events.PLAYER_CAREER_RESOURCE_UPDATED, "Hate.UpdateChoppa")
+    Hate.UpdateChoppa()
+    Hate.ActiveUpdater = "Choppa"
+  elseif  GameData.Player.career.line == GameData.CareerLine.SORCERER or GameData.Player.career.line == GameData.CareerLine.BRIGHT_WIZARD then
+    RegisterEventHandler(SystemData.Events.PLAYER_CAREER_RESOURCE_UPDATED, "Hate.UpdateSorc")
+    Hate.UpdateSorc()
+    Hate.ActiveUpdater = "Sorc"
+  end
 end
 
 function Hate.ShowHateWindow()
@@ -20,29 +54,78 @@ function Hate.ShowHateWindow()
   LayoutEditor.RegisterWindow("HateWindow", L"Hate", L"Hate", true, true, true, nil)
 end
 
-function Hate.Update()
+function Hate.UpdateDoC()
   if not Hate.CareerAllowed then return end
   local Resource = GetCareerResource(GameData.BuffTargetType.SELF)
   LabelSetText("HateWindowCounter", towstring(Resource))
 
-  local Stage1 = math.floor(Resource / 25)
-  local Stage2 = math.floor(Resource / 30)
+  if Resource <= 100 then
+    LabelSetTextColor("HateWindowCounter", 200, 0, 0)
+  elseif Resource > 100 and Resource < 200 then
+    LabelSetTextColor("HateWindowCounter", 255, 255, 0)
+  else
+    LabelSetTextColor("HateWindowCounter", 255, 255, 255)
+  end
+end
 
-  LabelSetText("HateWindowStage1", towstring(Stage1))
-  LabelSetText("HateWindowStage2", towstring(Stage2))
+function Hate.UpdateWE()
+  if not Hate.CareerAllowed then return end
+  local Resource = GetCareerResource(GameData.BuffTargetType.SELF)
+  LabelSetText("HateWindowCounter", towstring(Resource))
+
+  if Resource == 5 then
+     LabelSetTextColor("HateWindowCounter", 200, 0, 0)
+  elseif Resource < 5 then
+    LabelSetTextColor("HateWindowCounter", 255, 255, 255)
+  end
+end
+
+function Hate.UpdateChoppa()
+  if not Hate.CareerAllowed then return end
+  local Resource = GetCareerResource(GameData.BuffTargetType.SELF)
+  LabelSetText("HateWindowCounter", towstring(Resource))
+
+  if Resource >= 75 then
+     LabelSetTextColor("HateWindowCounter", 200, 0, 0)
+  elseif Resource < 75 and Resource >= 25 then
+    LabelSetTextColor("HateWindowCounter", 255, 255, 0)
+  else
+    LabelSetTextColor("HateWindowCounter", 255, 255, 255)
+  end
+end
+
+function Hate.UpdateSorc()
+  if not Hate.CareerAllowed then return end
+  local Resource = GetCareerResource(GameData.BuffTargetType.SELF)
+  LabelSetText("HateWindowCounter", towstring(Resource))
+end
+
+function Hate.UpdateBG()
+  local Resource = GetCareerResource(GameData.BuffTargetType.SELF)
+  LabelSetText("HateWindowCounter", towstring(Resource))
+
+  if Resource == 100 then
+    LabelSetTextColor("HateWindowCounter", 255, 100, 255)
+  elseif Resource >= 75 then
+     LabelSetTextColor("HateWindowCounter", 200, 0, 0)
+  elseif Resource >= 50 then
+    LabelSetTextColor("HateWindowCounter", 255, 150, 10)
+  elseif Resource >= 25 then
+    LabelSetTextColor("HateWindowCounter", 255, 255, 0)
+  else
+    LabelSetTextColor("HateWindowCounter", 255, 255, 255)
+  end
 end
 
 function Hate.Shutdown()
   UnregisterEventHandler(SystemData.Events.ENTER_WORLD, "Hate.Enable")
   UnregisterEventHandler(SystemData.Events.INTERFACE_RELOADED, "Hate.Enable")
   if not Hate.CareerAllowed then return end
-  UnregisterEventHandler(SystemData.Events.PLAYER_CAREER_RESOURCE_UPDATED, "Hate.Update")
+
+  funcName = string.format("Hate.Update%s", Hate.ActiveUpdater)
+  UnregisterEventHandler(SystemData.Events.PLAYER_CAREER_RESOURCE_UPDATED, funcName)
 end
 
 function Hate.isCareerAllowed()
-  if GameData.Player.career.line == GameData.CareerLine.BLACKGUARD or GameData.Player.career.line == GameData.CareerLine.IRON_BREAKER then
-    return true
-  else
-    return false
-  end
+  return Hate.AllowedCareers[GameData.Player.career.line] or false
 end
--- Hate.mod
+++ Hate.mod
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <ModuleFile xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
-    <UiMod name="Hate" version="0.22" date="04/28/2020" >
+    <UiMod name="Hate" version="1.0" date="10/08/2025" >
 
         <Author name="dimakss" email="1dimakss@gmail.com" />
         <Description text="HateCounter" />
@@ -15,6 +15,14 @@
           <Careers>
             <Career name="BLACKGUARD" />
             <Career name="IRON_BREAKER" />
+            <Career name="DISCIPLE_OF_KHAINE" />
+            <Career name="WARRIOR_PRIEST" />
+            <Career name="WITCH_ELF" />
+            <Career name="WITCH_HUNTER" />
+            <Career name="SORCERER" />
+            <Career name="BRIGHT_WIZARD" />
+            <Career name="CHOPPA" />
+            <Career name="SLAYER" />
           </Careers>
         </WARInfo>
 
